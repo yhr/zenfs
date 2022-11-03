6653ec3fc8de9468ef434b4571cbfa4cd3951ff6
Hans Holmberg <hans.holmberg@wdc.com>
2022-11-03 08:58:09 +0000

    fio_plugin: stash xfs zenfs script update

--- fio_plugin/compare_xfs_zenfs.sh
+++ fio_plugin/compare_xfs_zenfs.sh
@@ -2,14 +2,19 @@
 set -e
 
 FIO_SCRIPT=$1
-
+EXTRA_PARAMETERS="$2 $3 $4 $5 $6 $7 $8"
 FIO_COMMON="fio"
 ROCKSDB_IOENGINE="--ioengine=./rocksdbfs_fio_engine.so"
 
 echo Running $FIO_SCRIPT on top of ZENFS
-$FIO_COMMON $ROCKSDB_IOENGINE --fs_uri=zenfs://dev:nvme2n1                               $FIO_SCRIPT | tee $FIO_SCRIPT-zenfs.log
-echo Running $FIO_SCRIPT on top of XFS
-$FIO_COMMON $ROCKSDB_IOENGINE --fs_uri=posix://             --directory=/mnt/xfs_nvme1n1 $FIO_SCRIPT | tee $FIO_SCRIPT-xfs_posix.log
+$FIO_COMMON $ROCKSDB_IOENGINE --fs_uri=zenfs://dev:nvme2n1 $EXTRA_PARAMETERS $FIO_SCRIPT | tee $FIO_SCRIPT-zenfs.log
 
-vimdiff $FIO_SCRIPT-xfs_posix.log $FIO_SCRIPT-zenfs.log
+#echo Running $FIO_SCRIPT on top of XFS (rocksdb io engine)
+#$FIO_COMMON $ROCKSDB_IOENGINE --fs_uri=posix://             --directory=/mnt/xfs_nvme1n1 $FIO_SCRIPT | tee $FIO_SCRIPT-xfs_posix.log
+
+XFS_IOENGINE=libaio
+echo Running $FIO_SCRIPT on top of XFS
+$FIO_COMMON --directory=/mnt/xfs_nvme1n1 --ioengine=$XFS_IOENGINE $EXTRA_PARAMETERS $FIO_SCRIPT | tee $FIO_SCRIPT-xfs_libaio.log
+
+vimdiff $FIO_SCRIPT-xfs_$XFS_IOENGINE.log $FIO_SCRIPT-zenfs.log
 
