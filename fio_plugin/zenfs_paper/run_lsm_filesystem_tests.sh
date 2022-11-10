f3d1e77aa833f5f864f49b7a6634880a75f4298d
Hans Holmberg <hans.holmberg@wdc.com>
2022-11-10 07:56:35 +0000

    lsm filesystem test script update

--- fio_plugin/zenfs_paper/run_lsm_filesystem_tests.sh
+++ fio_plugin/zenfs_paper/run_lsm_filesystem_tests.sh
@@ -5,23 +5,25 @@ FS="$1"
 FS_PARAMETERS="$2 $3 $4 $5 $6 $8 $9"
 DEV_BS=4096
 
+FIO_SCRIPT="wal_performance.fio"
+for I in {0..7} # 200b..16000b
+do
+  BS=$((200 * (2 ** $I)))
+  DATASYNC=1
+    
+  echo "# Testing wal performance blocksize $BS bytes fs: $FS"
+ 
+  ./run_fio_test.sh "$FS-wal-log" "$FIO_SCRIPT" "$FS_PARAMETERS" "$BS" "--fdatasync=$DATASYNC --direct=0 --filename=test.log"
+  ./run_fio_test.sh "$FS-wal-dat" "$FIO_SCRIPT" "$FS_PARAMETERS" "$BS" "--fdatasync=$DATASYNC --direct=0 --filename=test.dat"
+
+done
+
+
 FIO_SCRIPT="sst_performance.fio"
 DATASYNC=0
 for DIRECT in {0..1}
 do
   echo "# Testing sst performance direct=$DIRECT"
   ./run_fio_test.sh "$FS-sst-direct=$DIRECT" "$FIO_SCRIPT" "$FS_PARAMETERS" "NA" "--fdatasync=$DATASYNC --direct=$DIRECT"
-done
-
-FIO_SCRIPT="wal_performance.fio"
-for I in {0..7} # 256b..16k
-do
-  BS=$((256 * (2 ** $I)))
-  DATASYNC=1
-    
-  echo "# Testing wal performance blocksize $BS bytes fs: $FS"
- 
-  ./run_fio_test.sh "$FS-wal" "$FIO_SCRIPT" "$FS_PARAMETERS" "$BS" "--fdatasync=$DATASYNC --direct=0 --filename=test.log"
-
 done
 
